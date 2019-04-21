c3dc33fcee89ea682545551b2b7cc0689e6bdecb
mwalto7 <dev.mwalto7@gmail.com>
2019-04-21 16:49:20 -0500

    Updated the constructor and simplified the code.

--- contracts/Will.sol
+++ contracts/Will.sol
@@ -6,75 +6,80 @@ pragma solidity >=0.5.0;
  * @author Mason Walton, Michael Christian, Lex Adams
  */
 contract Will {
-    uint private amount;           // The amount of ether to transfer to the receiver.
-    uint private deadline;         // The deadline for Jack to withdraw the ethers.
-    address private attorney;      // The attorney's address.
-    address payable private jack;  // Jack's address.
+    uint private amount;           // The amount of ether to transfer to the recipient.
+    uint private deadline;         // The deadline by which Jack must withdraw the ethers.
     address payable private ngo;   // The NGO's address.
+    address payable private jack;  // Jack's address.
   	bytes32 private attorneyPass;  // The attorney's password part.
     bytes32 private recipientPass; // The recipient's password part.
     
-    // Ensure the caller is Jack.
-    modifier jackOnly {
-      	require(msg.sender == jack, "Permission Denied: Must be Jack");
+    // Ensure the sender is Jack.
+    modifier onlyJack {
+      	require(msg.sender == jack, "Sender must be Jack");
         _;
     }
     
-    // Ensure the caller is NGO.
-    modifier ngoOnly {
-      	require(msg.sender == ngo, "Permission Denied: Must be NGO");
+    // Ensure the sender is NGO.
+    modifier onlyNGO {
+      	require(msg.sender == ngo, "Sender must be NGO");
         _;
     }
   
   	// Ensure it is before the deadline.
     modifier beforeDeadline {
-      	require(deadline > now, "Permission Denied: After deadline");
+      	require(now < deadline, "Must be before deadline");
         _;
     }
   
   	// Ensure it is after the deadline.
     modifier afterDeadline {
-      	require(now > deadline, "Permission Denied: Before deadline");
+      	require(now >= deadline, "Must be after deadline");
         _;
     }
   
   	// Ensure the password parts are valid.
     modifier validPass {
-      	require(attorneyPass == "hello", "Permission Denied: Invalid attorney password part");
-      	require(recipientPass == "world", "Permission Denied: Invalid recipient password part");
+      	require(attorneyPass == "hello", "Invalid attorney password");
+      	require(recipientPass == "world", "Invalid recipient password");
     	_;
     }
 
     /**
      * Create a new Will contract.
+     * @param _ngo The NGO's address.
+     * @param _jack Jack's address.
      * @param _amount The amount of ethers to transfer.
      * @param _deadline The deadline by which Jack must withdraw the ethers.
      * @param _attorneyPass The attorney's password part.
      * @param _recipientPass Jack or the NGO's password part.
      */
-    constructor(uint _amount, uint _deadline, bytes32 _attorneyPass, bytes32 _recipientPass) payable public {
+    constructor(address payable _ngo, address payable _jack, uint _amount, uint _deadline, bytes32 _attorneyPass, bytes32 _recipientPass) public payable {
+        ngo = _ngo;
+        jack = _jack;
         amount = _amount;
         deadline = _deadline;
         attorneyPass = _attorneyPass;
         recipientPass = _recipientPass;
-        // TODO: Set up addresses.
     }
     
     /**
-     * Transfers the account balance to Jack only is the deadline has not passed.
-     * @dev Only jack can perform this operation.
+     * Transfers the account balance to Jack.
+     *
+     * @dev Only Jack can perform this operation, and only if it is before the
+     * deadline and his password part is valid.
      */
-    function withdraw() jackOnly beforeDeadline validPass public {
+    function withdraw() public payable onlyJack beforeDeadline validPass {
         jack.transfer(amount);
     }
     
     /**
-     * Transfers the account balance to NGO and cancels the contract
-     * only after the deadline has passed.
-     * @dev Only the NGO can perform this operation.
+     * Transfers the account balance to NGO and cancels the contract.
+     *
+     * @dev Only the NGO can perform this operation, and only if it is after the
+     * deadline and their password part is valid.
      */
-    function cancelWithdraw() ngoOnly afterDeadline validPass public {
-    	ngo.transfer(amount);
+    function cancelWithdraw() public payable onlyNGO afterDeadline validPass {
+        ngo.transfer(amount);
         selfdestruct(ngo);
     }
 }
