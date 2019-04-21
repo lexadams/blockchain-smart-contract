f5bbe1924cdc08ffdcd2174e1349c5c281c86c10
mwalto7 <dev.mwalto7@gmail.com>
2019-04-21 17:34:38 -0500

    Updated test cases

--- test/will_test.js
+++ test/will_test.js
@@ -1,7 +1,11 @@
-contract('WillTest', function (accounts) {
-  it("should assert true", function (done) {
-    var will_test = WillTest.deployed();
-    assert.isTrue(true);
-    done();
+const Will = atrifacts.require("Will");
+
+contract("Will", function (accounts) {
+  it("should pass when both passwords are correct and deadline is not reached", function() {
+
+  });
+
+  it("should fail when one or both pieces of password are incorrect", function() {
+
   });
 });
